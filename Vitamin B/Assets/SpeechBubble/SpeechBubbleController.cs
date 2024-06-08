b6db1836ac7a3bb32d086c621bd8f0265d01f563
Damian-Lachenmayr <74145510+Damian-Lachenmayr@users.noreply.github.com>
2024-06-08 12:23:05 +0200

    merged content and controller

--- Vitamin B/Assets/SpeechBubble/SpeechBubbleController.cs
+++ Vitamin B/Assets/SpeechBubble/SpeechBubbleController.cs
@@ -1,15 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public enum Topics
+{
+    Business
+}
+
+public enum NoTopics
+{
+    Bitcoin,
+    NFTs,
+    Blockchain,
+    AI,
+    Crypto,
+    Microtransactions,
+    Battlepass,
+    Lifeservice,
+    Publisher,
+    Investors
+}
 
 public class SpeechBubbleController : MonoBehaviour
 {
     [SerializeField]
-    private SpeechBubbleContent _bubbleContent;
+    private float textFillSpeed = 0.5f;
+    
+    
+    
+    
+    private TMP_Text content;
+    private String targetName;
+    private int wordCount = 0;
+    private int wordMax = 8;
+    private int NoTopicsLength = Enum.GetNames(typeof(NoTopics)).Length;
 
     private void Awake()
     {
         //_bubbleContent.FillBubble();
+    }
+    
+    IEnumerator FillBubbleDelayed()
+    {
+        if (wordCount >= wordMax)
+            content.text = "";
+        
+        if(Random.Range(0, wordMax) == wordMax)
+            content.text.Append<>("Bla ");
+        wordCount++;
+        yield return new WaitForSeconds(textFillSpeed);
+        FillBubbleDelayed();
     }
 }
