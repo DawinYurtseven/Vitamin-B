13394ef08a5fc5315733c8f9cdbe3aa62f65d646
Damian-Lachenmayr <74145510+Damian-Lachenmayr@users.noreply.github.com>
2024-06-08 12:34:34 +0200

    bla

--- Vitamin B/Assets/SpeechBubble/SpeechBubbleController.cs
+++ Vitamin B/Assets/SpeechBubble/SpeechBubbleController.cs
@@ -29,15 +29,16 @@ public class SpeechBubbleController : MonoBehaviour
 {
     [SerializeField]
     private float textFillSpeed = 0.5f;
-    
-    
-    
-    
-    private TMP_Text content;
-    private String targetName;
+
+    private bool keywordSpawned = false;
     private int wordCount = 0;
     private int wordMax = 8;
     private int NoTopicsLength = Enum.GetNames(typeof(NoTopics)).Length;
+    
+    private TMP_Text content;
+    private String targetName;
+   
+    
 
     private void Awake()
     {
@@ -46,13 +47,33 @@ public class SpeechBubbleController : MonoBehaviour
     
     IEnumerator FillBubbleDelayed()
     {
-        if (wordCount >= wordMax)
+        if (wordCount > wordMax)
+        {
             content.text = "";
+            wordCount = 0;
+            keywordSpawned = false;
+        }
+
+        if (Random.Range(0 + wordCount, wordMax) == wordMax && !keywordSpawned)
+        {
+            string keyword = "";
+            if (Random.Range(0, 3) == 3)
+            {
+                keyword = targetName;
+            }
+            else
+            {
+                keyword = Enum.GetName(typeof(NoTopics), Random.Range(0, NoTopicsLength - 1));
+            }
+            keywordSpawned = true;
+        }
+        else
+        {
+            content.text.Concat("Bla ");
+        }
         
-        if(Random.Range(0, wordMax) == wordMax)
-            content.text.Append<>("Bla ");
         wordCount++;
         yield return new WaitForSeconds(textFillSpeed);
-        FillBubbleDelayed();
+        StartCoroutine(FillBubbleDelayed());
     }
 }
